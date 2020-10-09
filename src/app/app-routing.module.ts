3df8bcef1d648beb0b5b87c759ae7f22d1a2ea04
BluBluAdmin <blublu.main@gmail.com>
2020-10-09 19:29:07 +0200

    svg instagram/facebook/menu/twitter svg i app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { GamesComponent } from './games/games.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'games', component: GamesComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'contact', component: ContactComponent },
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { fadeInAnimation } from './route-animations';
 
 @Component({
   selector: 'blublu-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
-  animations: [fadeInAnimation],
 })
 export class AppComponent {
   title = 'blublu-web';
